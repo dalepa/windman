814e1807c4c284c0c3e53e38ad85239c987778a5
dalepa <dan.pancamo@gmail.com>
2024-03-19 10:35:41 -0500

    Update WindMan1.ino
    1.0

--- WindMan1/WindMan1.ino
+++ WindMan1/WindMan1.ino
@@ -252,7 +252,7 @@ float logBatteryLevel() {
 
 
   // Map the voltage to a percentage (replace with your specific battery curve if known)
-  float percentage = mapFloat(voltage, 3.3, 1.0, 100, 0);
+  float percentage = mapFloat(voltage, 1.92, 1.1, 100, 0);
 
   // Constrain the percentage to be between 0 and 100
  // percentage = constrain(percentage, 0, 100);
